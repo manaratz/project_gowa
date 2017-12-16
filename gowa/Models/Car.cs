6864a3d9945be6da460b2810ce768de13ac6caca
LUKSCH Frederick <luksch@et.esiea.fr>
2017-12-16 18:27:28 +0100

    name

---
+++ gowa/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gowa.Models
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public int CarWorkShopID { get; set; }
+
+        public virtual Workshop Workshop { get; set; }
+    }
+}
---
+++ gowa/Models/Workshop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gowa.Models
+{
+    public class Workshop
+    {
+        public int WorkshopID { get; set; }
+        public int Location { get; set; }
+        public string Name { get; set; }
+
+
+        public virtual ICollection<Car> Car { get; set; }
+    }
+}
