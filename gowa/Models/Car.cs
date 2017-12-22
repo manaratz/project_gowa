7aa955981554b8003a04f42f0898aa750035d4ee
LUKSCH Frederick <luksch@et.esiea.fr>
2017-12-22 21:32:46 +0100

    no negative horsepower

--- gowa/Models/Car.cs
+++ gowa/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace gowa.Models
     public class Car
     {
         public int ID { get; set; }
+        [Display(Name ="Workshop")]
         public int WorkShopID { get; set; }
+        [Display(Name ="Model")]
         public int ModelID { get; set; }
+        [Display(Name = "Horse Power"),Range(1,1000,ErrorMessage = "Please enter a value bigger than {1}")]
         public int HorsePower { get; set; }
 
         public virtual Workshop Workshop { get; set; }
